90bf15b62be6dc1ab690ca0e3038ae566d19231c
wonjangcloud9 <venuscwj@naver.com>
2023-02-05 21:16:21 +0900

    ini

--- app/src/main/java/com/example/jetpack_concept/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_concept/MainActivity.kt
@@ -22,22 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Text("Hello World!")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackconceptTheme {
-        Greeting("Android")
     }
 }
