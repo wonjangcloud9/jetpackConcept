3941dc5e1d307687e374f2eabdc7bb860f76308d
wonjangcloud9 <venuscwj@naver.com>
2023-02-05 21:23:11 +0900

    column & row & text

--- app/src/main/java/com/example/jetpack_concept/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_concept/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.jetpack_concept
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack_concept.ui.theme.JetpackconceptTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Text("Hello World!")
+                    Column(
+                        modifier = Modifier
+                            .background(color = MaterialTheme.colors.primary),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .background(color = MaterialTheme.colors.secondary)
+                                .padding(10.dp)
+                        ) {
+                            Text("Hello")
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text("World!")
+                        }
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Row(
+                            modifier = Modifier
+                                .background(color = MaterialTheme.colors.secondary)
+                                .padding(10.dp)
+                        ) {
+                            Text("Hello")
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text("World!")
+                        }
+                    }
                 }
             }
         }
