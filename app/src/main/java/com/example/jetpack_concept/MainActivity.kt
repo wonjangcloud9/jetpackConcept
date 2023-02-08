32c688a8ec089a254608c56e9a1f9ba8ab8fd793
wonjangcloud9 <venuscwj@naver.com>
2023-02-09 00:15:07 +0900

    import nav

--- app/src/main/java/com/example/jetpack_concept/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_concept/MainActivity.kt
@@ -3,32 +3,16 @@ package com.example.jetpack_concept
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.jetpack_concept.ui.theme.JetpackconceptTheme
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-            val scrollState = rememberScrollState()
+            val navController = rememberNavController()
 
-            LazyColumn(){
-
-            }
         }
     }
 }
